ab152b456b66b6f38a624bf6050b447f88af52b2
mark <markpackham1@gmail.com>
2024-01-18 11:07:17 +0000

    Add comment foreign ids for user and post
    https://youtu.be/oyMd6Du0ZDc?si=PlgNqKPrAsU3F__X&t=414

--- database/migrations/2024_01_18_110315_create_comments_table.php
+++ database/migrations/2024_01_18_110315_create_comments_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            // A user creates a comment so we need to restrict user deletions on their existing comments
+            $table->foreignIdFor(User::class)->constrained()->restrictOnDelete();
+            // A comment is associated with a post
+            $table->foreignIdFor(Post::class);
             $table->timestamps();
         });
     }
