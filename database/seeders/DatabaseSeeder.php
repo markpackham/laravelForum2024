ec70968f144033bceb1668ee2a2fa0d84958f517
mark <markpackham1@gmail.com>
2024-01-18 12:52:23 +0000

    Seed database with fake users
    https://youtu.be/4CAO7eA7mOk?feature=shared&t=387

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -10,9 +11,12 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+
+     // php artisan db:seed
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // Create 10 random users
+        $users = User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
