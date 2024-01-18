75c40cc5265ecdc06bf48f4bbc5ad7b9959b6436
mark <markpackham1@gmail.com>
2024-01-18 12:16:37 +0000

    Add User factory in PostFactory
    https://youtu.be/4CAO7eA7mOk?feature=shared&t=137

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,8 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            // Don't need to call create() since Laravel automatically only class it when necessary
+            'user_id' => User::factory(),
         ];
     }
 }
