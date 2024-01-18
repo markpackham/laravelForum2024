a64014e245eb4f3789ed0673ff78cc6ce0d3ab8d
mark <markpackham1@gmail.com>
2024-01-18 10:48:53 +0000

    Setup foreign key with user & restrict user deletion when tied to posts
    https://youtu.be/oyMd6Du0ZDc?si=mzQBbYI6AEvg_kAx&t=300

--- database/migrations/2024_01_18_103526_create_posts_table.php
+++ database/migrations/2024_01_18_103526_create_posts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            // Use fully qualified class name of a model rather than table name for foreignId
+            // this keeps things strict & less risk of change Vs table name
+
+            // Create database exception if we delete user who has posts via restrictOnDelete()
+            $table->foreignIdFor(User::class)->constrained()->restrictOnDelete();
             $table->string('title');
             $table->longText('body');
             $table->timestamps();
